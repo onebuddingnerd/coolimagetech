adaabaab0aeeba0a26c7ed288cab1654b2287203
onebuddingnerd <avik.rk.8@gmail.com>
2020-12-30 22:59:29 -0500

    file cleanup

--- working_signup_login.py
+++ working_signup_login.py
@@ -24,43 +24,6 @@ def get_diff(i1, i2):
 
     return sum(sum(abs(face_embeddings_i2 - face_embeddings_i1)))
 
-
-def get_diff1(i1, i2):
-    def cv2pil(cv):
-        colorconv_cv = cv2.cvtColor(cv, cv2.COLOR_BGR2RGB)
-        return Image.fromarray(colorconv_cv)
-
-    def pil2cv(pil):
-        cv2im = np.array(pil)
-        return cv2im[:,:,::-1] # reversing the z-axis (color channels: RGB -> BGR)
-
-    # resize the two images (smaller one unchanged, larger one shrunken)
-    ymax, xmax = min(i2.shape[0], i1.shape[0]), min(i2.shape[1], i1.shape[1])
-
-    i1_pil_resized = (cv2pil(i1)).resize((xmax,ymax)) 
-    i1_resized_cv = pil2cv(i1_pil_resized)
-
-    i2_pil_resized = (cv2pil(i2)).resize((xmax,ymax))
-    i2_resized_cv = pil2cv(i2_pil_resized)
-
-    # use mean value across channels
-    i1r, i1g, i1b = i1_resized_cv[:,:,0], i1_resized_cv[:,:,1], i1_resized_cv[:,:,2]
-    i1_1 = np.array([[(i1r[i,j] + i1g[i,j] + i1b[i,j])/3  for j in range (i1_resized_cv.shape[1])] for i in range(i1_resized_cv.shape[0])], dtype = 'int64')
-    i2r, i2g, i2b = i2_resized_cv[:,:,0], i2_resized_cv[:,:,1], i2_resized_cv[:,:,2]
-    i2_1 = np.array([[(i2r[i,j] + i2g[i,j] + i2b[i,j])/3  for j in range (i2_resized_cv.shape[1])] for i in range(i2_resized_cv.shape[0])], dtype = 'int64')
-
-    # The normalized Sum of Square Difference -- This is pretty bad in the cases I tested
-    # sq_diff = (i2_1-i1_1)**2
-    # sum_sq_dff = sum(sum(sq_diff))
-    # normalize = ((sum(sum(i1_1**2))) + (sum(sum(i1_1**2))))**(.5)
-
-    # The difference in the L2 Norms
-    L2_i1 = (sum(sum(i1_1**2)))**(0.5)
-    L2_i2 = (sum(sum(i2_1**2)))**(0.5)
-
-    return abs(L2_i2 - L2_i1)
-
-
 def compute_diff_scores(i1, i2): # params: full frame (a_face), crop (a_face_only)
     scores = []
     filenames = []
