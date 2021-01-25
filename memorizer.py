9a31a873f09463896badb238108da38ad4d5f7d6
onebuddingnerd <67214183+onebuddingnerd@users.noreply.github.com>
2021-01-24 22:19:28 -0500

    Create memorizer.py

---
+++ memorizer.py
@@ -0,0 +1,49 @@
+class UserData:
+
+    def __init__(self, name):
+        self.name = name
+        self.freq_data = {}
+        self.freq_data[1] = set()
+        self.list_current = []
+
+    def list_reset(self):
+        self.list_current = []
+
+    def find_freqkey(self, product):
+        key = None
+        for k in self.freq_data.keys():
+            if product in self.freq_data[k]:
+                k = key
+                break
+
+        return key
+
+    def is_recommended_product(self, product):
+        return self.find_freqkey() > 0
+
+    def freq_upgrade(product, freq_prev):
+        self.freq_data[freq_prev].remove(product)
+
+        if freq_prev + 1 in self.freq_data.keys():
+            self.freq_data[freq_prev + 1].add(product)
+        else:
+            self.freq_data[freq_prev + 1] = set()
+            self.freq_data[freq_prev + 1].add(product)
+
+    def add_product(self, product):
+        self.list_current.append(product)
+        product_freqkey = self.find_freqkey(product)
+        if not product_freqkey:
+            self.freq_data[1].add(product)
+        else:
+            self.freq_upgrade(product, product_freqkey)
+
+    def get_ordered_recs(self):
+        recs = []
+        for freq in sorted(list(self.freq_data.keys()), reverse = True):
+            recs.extend(self.freq_data[freq])
+
+        return recs
+
+
+
